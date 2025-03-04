1d03e355b3d35cabd75f83c20bd6876cbe4eb860
Krulciferd <johanmr633@gmail.com>
2025-03-04 18:27:32 -0500

    UIPauseButon
    creacion de paneles de GameOver y Pausa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject BotonPausa;
+   [SerializeField] private GameObject PausaPanel;
+   private bool GamePause = false;
+
    public void ChangeScene(string nameScene)
    {
     SceneManager.LoadScene(nameScene);
    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+         if(GamePause){
+            Reanudar();
+         }else{
+            Pausa();
+         }
+        }
+    }
+    public void Pausa()
+   {
+      GamePause = true;
+      Time.timeScale = 0f;
+      BotonPausa.SetActive(false);
+      PausaPanel.SetActive(true);
+   }
+
+   public void Reanudar()
+   {
+      GamePause = false;
+      Time.timeScale = 1f;
+      BotonPausa.SetActive(true);
+      PausaPanel.SetActive(false);
+   }
+
+   public void Reiniciar()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+   
 }
